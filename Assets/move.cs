4db013c98f450b8bbd5b7d74dfb283c91210b3b5
Luis Fernando da Silva <luis_fernandods@hotmail.com>
2018-09-03 22:11:17 -0300

    Commit Inicial

---
+++ Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class move : MonoBehaviour {
+
+	public SocketIOComponent socket;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		socket.Emit ("movendo", JSONObject.CreateStringObject(transform.position.ToString() + transform.rotation.ToString()) );
+
+	}
+}
